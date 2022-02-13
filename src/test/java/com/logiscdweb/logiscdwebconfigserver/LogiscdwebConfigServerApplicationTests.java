2819fe89c2c03651d2af444b856be28534330f22
Nicole <zenanicole1412@gmail.com>
2022-02-13 15:25:49 -0700

    first commit

---
+++ src/test/java/com/logiscdweb/logiscdwebconfigserver/LogiscdwebConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiscdweb.logiscdwebconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiscdwebConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
